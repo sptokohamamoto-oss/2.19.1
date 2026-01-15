31fc5669ae54271ac9a97de755b1b3814c6dfc38
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-15 16:39:02 +0900

    a

--- ConsoleApp19-1/Program.cs
+++ ConsoleApp19-1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ConsoleApp19_1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            TestPerson person = new Testinfo("山田", 20, 170, 60);
+        }
+
+    }
+
+    class TestPerson
+    {
+        string name;
+        int age;
+        public TestPerson(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Console.WriteLine("基底クラス");
+            Console.WriteLine("名前:" + name + ",年齢" + age);
+        }
+    }
+    class Testinfo : TestPerson
+    {
+        int height;
+        int weight;
+        public Testinfo(string name, int age, int height, int weight) : base(name, age)
+        {
+            this.height = height;
+            this.weight = weight;
+            Console.WriteLine("派生クラス");
+            Console.WriteLine("身長:" + height + ",体重" + weight);
+        }
+
+    }
+}
